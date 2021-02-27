5b0df2e1bd178c1d4f4f2116dd5bfe4f821bb3b1
jonath92 <jonathanheard92@gmail.com>
2021-02-27 10:02:54 +0100

    initial commit

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -7,7 +7,6 @@ import {
   incrementAsync,
   selectCount,
 } from './counterSlice';
-import styles from './Counter.module.css';
 
 export function Counter() {
   const count = useSelector(selectCount);
@@ -16,32 +15,25 @@ export function Counter() {
 
   return (
     <div>
-      <div className={styles.row}>
+      <div >
         <button
-          className={styles.button}
-          aria-label="Increment value"
           onClick={() => dispatch(increment())}
         >
           +
         </button>
-        <span className={styles.value}>{count}</span>
+        <span>{count}</span>
         <button
-          className={styles.button}
-          aria-label="Decrement value"
           onClick={() => dispatch(decrement())}
         >
           -
         </button>
       </div>
-      <div className={styles.row}>
+      <div>
         <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
           value={incrementAmount}
           onChange={e => setIncrementAmount(e.target.value)}
         />
         <button
-          className={styles.button}
           onClick={() =>
             dispatch(incrementByAmount(Number(incrementAmount) || 0))
           }
@@ -49,7 +41,6 @@ export function Counter() {
           Add Amount
         </button>
         <button
-          className={styles.asyncButton}
           onClick={() => dispatch(incrementAsync(Number(incrementAmount) || 0))}
         >
           Add Async
